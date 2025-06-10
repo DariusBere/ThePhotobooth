a9c1909af58b7bb36e9bb9b46692fc51d2faf5b8
DariusBere <dariusbere8@gmail.com>
2025-06-11 00:15:19 +0200

    Just the footer left

--- js/galeria.js
+++ js/galeria.js
@@ -1,14 +1,32 @@
 const imagenes = [
-    { src: 'img/foto1.jpg', alt: 'Atardecer en la montaña' },
-    { src: 'img/foto2.jpg', alt: 'Retrato en estudio' },
-    { src: 'img/foto3.jpg', alt: 'Ciudad de noche' },
-    { src: 'img/foto4.jpg', alt: 'Paisaje costero' },
-    { src: 'img/foto5.jpg', alt: 'Blossom Park' },
-    { src: 'img/foto6.jpg', alt: 'Arquitectura moderna' }
+    { src: '../img/gallery/foto1.jpg', alt: 'Cigarro' },
+    { src: '../img/gallery/foto2.jpg', alt: 'Telefono' },
+    { src: '../img/gallery/foto3.jpg', alt: 'Cueva' },
+    { src: '../img/gallery/foto4.jpg', alt: 'Boda a la orilla del mar' },
+    { src: '../img/gallery/foto5.jpg', alt: 'Bomba de humo' },
+    { src: '../img/gallery/foto6.jpg', alt: 'CocaCola' },
+    { src: '../img/gallery/foto7.jpg', alt: 'Serum' },
+    { src: '../img/gallery/foto8.jpg', alt: 'Autovia' },
+    { src: '../img/gallery/foto9.jpg', alt: 'Cortina de luces' },
+    { src: '../img/gallery/foto10.jpg', alt: 'Pine forest' },
+    { src: '../img/gallery/foto11.jpg', alt: 'Eucalyptus' },
+    { src: '../img/gallery/foto12.jpg', alt: 'Paseo en la montaña' },
+    { src: '../img/gallery/foto13.jpg', alt: 'Puente' },
+    { src: '../img/gallery/foto14.jpg', alt: 'Claudia' },
+    { src: '../img/gallery/foto15.jpg', alt: 'Sunset' },
+    { src: '../img/gallery/foto16.jpg', alt: 'Escalera natural' },
+    { src: '../img/gallery/foto17.jpg', alt: 'Coach' },
+    { src: '../img/gallery/foto18.jpg', alt: 'Adrian y Maria' },
+    { src: '../img/gallery/foto19.jpg', alt: 'La isla' },
+    { src: '../img/gallery/foto20.jpg', alt: 'Black Monochrome' },
+    { src: '../img/gallery/foto21.jpg', alt: 'Sunglasses' },
+    { src: '../img/gallery/foto22.jpg', alt: 'Anillos de boda' },
+    { src: '../img/gallery/foto23.jpg', alt: 'Andreea y Darius' },
+    { src: '../img/gallery/foto24.jpg', alt: 'El beso' },
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
-    const contenedor = document.getElementById('lista-galeria');
+    const contenedor = document.getElementById("lista-galeria");
 
     imagenes.forEach((img, idx) => {
         
@@ -20,20 +38,20 @@ document.addEventListener('DOMContentLoaded', () => {
         imagenElemento.alt = img.alt;
         imagenElemento.className = 'img-fluid rounded shadow-sm';
         imagenElemento.style.cursor = 'pointer';
-        imagenElemento.dataset.bsToggle = 'modal';
-        imagenElemento.dataset.bsTarget = `#modalFoto${idx}`;
+        imagenElemento.setAttribute('data-bs-toggle', 'modal');
+        imagenElemento.setAttribute('data-bs-target', `#modalFoto${idx}`);
 
         const modal = document.createElement('div');
         modal.className = 'modal fade';
         modal.id = `modalFoto${idx}`;
         modal.tabIndex = -1;
         modal.innerHTML = `
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
+            <div class="modal-dialog modal-dialog-centered modal-lg">
+                <div class="modal-content bg-transparent border-0">
                     <div class="modal-body p-0">
                         <img src="${img.src}" alt="${img.alt}" class="img-fluid w-100">
                     </div>
-                    <div class="modal-footer">
+                    <div class="modal-footer justify-content-center border-0">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
                     </div>
                 </div>
